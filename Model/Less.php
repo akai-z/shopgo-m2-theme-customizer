34594a16aceb1accef49f1b0ea20b32cbaf1c4a7
am-shopgo <ammar@shopgo.me>
2016-01-02 19:28:03 +0200

    Add LESS model

---
+++ Model/Less.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © 2015 ShopGo. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShopGo\ThemeCustomizer\Model;
+
+/**
+ * A model for handling LESS content
+ */
+class Less
+{
+    //TODO
+}
