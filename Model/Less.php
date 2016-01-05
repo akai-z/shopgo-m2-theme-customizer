6b30dc6cc2d7335474ff90232d8c945df541bd13
am-shopgo <ammar@shopgo.me>
2016-01-05 23:33:15 +0200

    Add less files parsing functionality

--- Model/Less.php
+++ Model/Less.php
@@ -5,10 +5,395 @@
  */
 namespace ShopGo\ThemeCustomizer\Model;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
+
 /**
- * A model for handling LESS content
+ * Less model
  */
-class Less
+class Less extends \Magento\Framework\Model\AbstractModel
 {
-    //TODO
+    /**
+     * Design frontend path
+     */
+    #const DESIGN_FRONTEND_PATH = 'design/frontend/Shopgo';
+
+    /**
+     * Pub frontend path
+     */
+    const PUB_FRONTEND_PATH = 'frontend/ShopGo';
+
+    /**
+     * Design frontend fields LESS file path
+     */
+    #const DESIGN_FIELDS_FILE_PATH = 'frontend/ShopGo';
+
+    /**
+     * Var theme customizer path
+     */
+    const VAR_THEME_CUSTOMIZER_PATH = 'shopgo/theme_customizer';
+
+    /**
+     * Pub static frontend fields LESS file path
+     */
+    #const PUB_FIELDS_FILE_PATH = 'frontend/ShopGo';
+
+    /**
+     * Fields LESS file path
+     */
+    const FIELDS_FILE_PATH = 'customizer/_fields.less';
+
+    /**
+     * Fields custom LESS file path
+     */
+    #const FIELDS_CUSTOM_FILE_PATH = '_fields_custom.less';
+
+    /**
+     * Var source LESS file path
+     */
+    const VAR_SOURCE_FILE_PATH = 'customizer/_var_source.less';
+
+    /**
+     * Fields LESS section separator
+     */
+    const FIELDS_SECTION_SEPARATOR = '//========';
+
+    /**
+     * LESS var separator
+     */
+    const LESS_VAR_SEPARATOR = '//---';
+
+    /**
+     * Fields LESS var child separator
+     */
+    const FIELDS_VAR_CHILD_SEPARATOR = '//>';
+
+    /**
+     * LESS var description label
+     */
+    const LESS_VAR_DESCRIPTION_LABEL = '//Description:';
+
+    /**
+     * @var \Magento\Framework\Filesystem
+     */
+    protected $_filesystem;
+
+    /**
+     * @var \Magento\Framework\Filesystem\Directory\ReadInterface
+     */
+    protected $_varDirectory;
+
+    /**
+     * @var \Magento\Framework\Filesystem\Directory\ReadInterface
+     */
+    #protected $_designFrontendDirectory;
+
+    /**
+     * @var \Magento\Framework\Filesystem\Directory\ReadInterface
+     */
+    #protected $_pubFrontendDirectory;
+
+    /**
+     * @var string
+     */
+    protected $_theme;
+
+    /**
+     * @var string
+     */
+    private $_varSection;
+
+    /**
+     * @param \Magento\Framework\Filesystem $filesystem
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->_filesystem = $filesystem;
+        $this->_setVarDirectory();
+        
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * Set Var directory
+     */
+    protected function _setVarDirectory()
+    {
+        $this->_varDirectory = $this->_filesystem
+            ->getDirectoryRead(DirectoryList::VAR_DIR . '/' . self::VAR_THEME_CUSTOMIZER_PATH);
+    }
+
+    /**
+     * Set design frontend directory
+     */
+    /*protected function _setDesignFrontendDirectory()
+    {
+        $this->_designFrontendDirectory = $this->_filesystem
+            ->getDirectoryRead(DirectoryList::APP . self::DESIGN_FRONTEND_PATH);
+    }*/
+
+    /**
+     * Set pub frontend directory
+     */
+    /*protected function _setPubFrontendDirectory()
+    {
+        $this->_pubFrontendDirectory = $this->_filesystem
+            ->getDirectoryRead(DirectoryList::STATIC_VIEW . self::PUB_FRONTEND_PATH);
+    }*/
+
+    /**
+     * Check whether fields LESS file exists
+     *
+     * @return boolean
+     */
+    protected function _fieldsFileExists()
+    {
+        return $this->_varDirectory->isFile(
+            $this->_theme . '/' . self::FIELDS_FILE_PATH
+        );
+    }
+
+    /**
+     * Get fields LESS file content
+     *
+     * @return boolean
+     */
+    protected function _getFieldsLessContent()
+    {
+        return $this->_varDirectory->readFile(
+            $this->_theme . '/' . self::DESIGN_FIELDS_FILE_PATH
+        );
+    }
+
+    /**
+     * Check whether fields custom LESS file exists
+     *
+     * @return boolean
+     */
+    /*protected function _fieldsCustomFileExists()
+    {
+        return $this->_varDirectory->isFile(
+            $this->_theme . '/' . self::FIELDS_CUSTOM_FILE_PATH
+        );
+    }*/
+
+    /**
+     * Get fields custom LESS file content
+     *
+     * @return string
+     */
+    /*protected function _getFieldsCustomLessContent()
+    {
+        return $this->_varDirectory->readFile(
+            $this->_theme . '/' . self::FIELDS_CUSTOM_FILE_PATH
+        );
+    }*/
+
+    /**
+     * Check whether fields source LESS file exists
+     *
+     * @return boolean
+     */
+    protected function _fieldsSourceFileExists()
+    {
+        return $this->_varDirectory->isFile(
+            $this->_theme . '/' . self::FIELDS_SOURCE_FILE_PATH
+        );
+    }
+
+    /**
+     * Get fields source LESS file content
+     *
+     * @return string
+     */
+    protected function _getFieldsSourceLessContent()
+    {
+        return $this->_varDirectory->readFile(
+            $this->_theme . '/' . self::FIELDS_Source_FILE_PATH
+        );
+    }
+
+    /**
+     * Get fields LESS variables sections
+     *
+     * @param string $fields
+     * @return array
+     */
+    protected function _getFieldsSections($fields)
+    {
+        $sections  = [];
+        $_sections = array_map('trim', explode(self::FIELDS_SECTION_SEPARATOR, $fields));
+
+        foreach ($_sections as $section) {
+            if (empty($section)) {
+                continue;
+            }
+
+            $_section = $this->_getFieldsVars($section);
+            $sections[$this->_varSection] = $_section;
+        }
+
+        return $sections;
+    }
+
+    /**
+     * Get fields LESS variables
+     *
+     * @param string $_vars
+     * @param string $varSeparator
+     * @param string $childSeparator
+     * @return array
+     */
+    protected function _getFieldsVars(
+        $_vars,
+        $varSeparator = self::LESS_VAR_SEPARATOR,
+        $childSeparator = self::FIELDS_VAR_CHILD_SEPARATOR
+    ) {
+        $vars  = [];
+        $_vars = array_map('trim', explode($varSeparator, $_vars));
+
+        foreach ($_vars as $index => $var) {
+            if (empty($var)) {
+                continue;
+            }
+
+            $vars[$index] = [];
+
+            if (strpos($var, $childSeparator) !== false) {
+                $subVars = array_map('trim', explode($childSeparator, $var));
+                $varParts = $this->_getVarParts($subVars[0]);
+
+                if (empty($varParts)) {
+                    continue;
+                }
+
+                if (strpos($subVars[1], $childSeparator) === false) {
+                    $varSeparator = PHP_EOL;
+                }
+
+                $vars[$index]['children'] = $this->_getFieldsVars(
+                    $subVars[1],
+                    $varSeparator,
+                    $childSeparator . '>'
+                );
+            } else {
+                $varParts = $this->_getVarParts($var);
+
+                if (empty($varParts)) {
+                    continue;
+                }
+            }
+
+            $vars[$index] = array_merge($vars[$index], $varParts);
+        }
+
+        return $vars;
+    }
+
+    /**
+     * Get fields LESS variable parts
+     *
+     * @param string $var
+     * @return array
+     */
+    protected function _getVarParts($var)
+    {
+        $parts  = [];
+        $_parts = array_map('trim', explode(PHP_EOL, $var));
+
+        if (empty($_parts)) {
+            return $parts;
+        }
+
+        $partsCount = count($_parts);
+
+        $_varVal = array_map('trim', explode(':', $_parts[$partsCount - 1]));
+        $_var    = explode('-', ltrim($_varVal[0], '@'));
+
+        if (count($_var) < 3) {
+            return $parts;
+        }
+
+        if ($partsCount > 1) {
+            $parts['description'] = trim(substr(
+                $_parts[0],
+                strlen(self::LESS_VAR_DESCRIPTION_LABEL),
+                strlen($_parts[0])
+            ));
+        }
+
+        $parts['type']  = $_var[1];
+        $parts['label'] = $_var[2];
+        $parts['value'] = $_varVal[1];
+
+        $this->_varSection = $_var[0];
+
+        return $parts;
+    }
+
+    /**
+     * Parse fields LESS file
+     *
+     * @return array
+     */
+    public function parseFieldsLess()
+    {
+        //if (!$this->_fieldsFileExists()) {
+            //return [];
+        //}
+
+        $less = $this->_getFieldsLessContent();
+        
+        //if ($lessCustom = $this->_getFieldsCustomLessContent()) {
+            //$less = arra_merge($less, $lessCustom);
+        //}
+
+        return $this->_getFieldsSections($less);
+    }
+
+    /**
+     * Parse fields source LESS file
+     *
+     * @return array
+     */
+    public function parseFieldsSourceLess()
+    {
+        //if (!$this->_fieldsSourceLessFileExists()) {
+            //return [];
+        //}
+
+        $less  = $this->_getFieldsSourceLessContent();
+        $_vars = array_map('trim', explode(self::LESS_VAR_SEPARATOR, $less));
+        $vars  = [];
+        
+        foreach ($_vars as $var) {
+            $varParts = $this->_getVarParts($var);
+            
+            if (!empty($varParts)) {
+                $vars[$this->_varSection] = $varParts;
+            }
+        }
+
+        return $vars;
+    }
+
+    /**
+     * Set theme
+     * 
+     * @param string $theme
+     */
+    public function setTheme($theme)
+    {
+        $this->_theme = $theme;
+    }
 }
